583d26562a935c0d0c2ba2036ed3cfc423495d6a
Pavel Yevtushenko <pavel.evtushenko@technorely.com>
2023-07-03 20:13:30 +0300

    add individual listing page

--- app/types/index.ts
+++ app/types/index.ts
@@ -1,4 +1,4 @@
-import { User } from "@prisma/client";
+import { Listing, User } from "@prisma/client";
 export type SafeUser = Omit<
   User,
   "createdAt" | "updatedAt" | "emailVerified"
@@ -6,4 +6,8 @@ export type SafeUser = Omit<
   createdAt: string;
   updatedAt: string;
   emailVerified: string | null;
+};
+
+export type SafeListing = Omit<Listing, "createdAt"> & {
+  createdAt: string;
 };
